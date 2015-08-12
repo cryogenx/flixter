20653a72502418de9c659245e3df6d37f513d510
George Welsted <george@gwelsted.com>
2015-08-12 01:39:05 +0000

    lesson 27 enrollments issue

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,23 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_course_enrollment_for_lessons, :only => [:show]
+
 	def show
+
 	end
 
-  private
+  	private
+
+  	def require_course_enrollment_for_lessons
+  		the_course = current_lesson.section.course
+
+    	if current_user.enrolled_in?(the_course) != true
+    		if current_lesson.section.course.user != current_user
+      			redirect_to course_path(the_course), alert: 'Please enroll to access lessons'
+      		end
+    	end
+	end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])

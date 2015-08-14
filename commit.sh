6a95a412bcb3829442012e5972526ffa0bf64d97
George Welsted <george@gwelsted.com>
2015-08-14 02:48:21 +0000

    updated commit.sh with some instructional comments

--- commit.sh
+++ commit.sh
@@ -2,6 +2,12 @@ echo "Please enter a commit description: "
 read input_variable
 echo "You entered: $input_variable"
 
+# Dumping of seed file requires gem 'seed_dump'
+
+# install seed dump by adding: gem 'seed_dump' to your project gemfile
+# then run bundle install
+
+# to dump a seed file uncomment the following line to disable seed dumps comment it out:
 rake db:seed:dump
 
 git add --all

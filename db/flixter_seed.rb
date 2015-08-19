20c7cf43cc8a6825f8098e70a6d24777e0f1b88b
George Welsted <george@gwelsted.com>
2015-08-19 03:15:01 +0000

    added section modal

--- db/flixter_seed.rb
+++ db/flixter_seed.rb
@@ -5,12 +5,12 @@ Course.create!([
 Section.create!([
   {id: 1, title: "New Beginnings", course_id: 1, created_at: "2015-08-19 01:38:05", updated_at: "2015-08-19 01:38:05", row_order: 2796203},
   {id: 2, title: "Html 5", course_id: 1, created_at: "2015-08-19 01:38:05", updated_at: "2015-08-19 01:38:05", row_order: -2796202},
-  {id: 3, title: "CSS", course_id: 1, created_at: "2015-08-19 01:38:05", updated_at: "2015-08-19 01:38:05", row_order: 4194304},
   {id: 4, title: "Section 0", course_id: 2, created_at: "2015-08-19 01:38:05", updated_at: "2015-08-19 01:38:05", row_order: 0},
   {id: 5, title: "Section 1", course_id: 2, created_at: "2015-08-19 01:38:05", updated_at: "2015-08-19 01:38:05", row_order: 4194304},
   {id: 6, title: "Section 2", course_id: 2, created_at: "2015-08-19 01:38:05", updated_at: "2015-08-19 01:38:05", row_order: 6291456},
   {id: 7, title: "Section 3", course_id: 2, created_at: "2015-08-19 01:38:05", updated_at: "2015-08-19 01:38:05", row_order: 7340032},
-  {id: 8, title: "Section 4", course_id: 2, created_at: "2015-08-19 01:38:05", updated_at: "2015-08-19 01:38:43", row_order: 7864320}
+  {id: 8, title: "Section 4", course_id: 2, created_at: "2015-08-19 01:38:05", updated_at: "2015-08-19 01:38:43", row_order: 7864320},
+  {id: 3, title: "CSS", course_id: 1, created_at: "2015-08-19 01:38:05", updated_at: "2015-08-19 03:05:00", row_order: 1}
 ])
 Lesson.create!([
   {id: 31, title: "Basic Building Blocks", subtitle: "This lesson teaches you the basics of HTML code and how to start building new pages", section_id: 2, created_at: "2015-08-19 01:38:15", updated_at: "2015-08-19 01:38:15", video: nil, row_order: 0},
